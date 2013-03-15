81f75835a7e537daffe72827d055d68ebd1c914b
Sarah <sarah.bernelind@gmail.com>
2013-03-15 14:33:48 +0100

    lagt till metoder i floor

--- src/Building.java
+++ src/Building.java
@@ -11,14 +11,14 @@ public class Building {
 	private ArrayList<Floor> floorList;
 
 
-	public Building(int nrfloors, int nrElevators) { //id = vilken vÃ¥ning, maxFloor = vilken Ã¶versta vÃ¥ningen Ã¤r
+	public Building(int nrfloors, int nrElevators) { //id = vilken våning, maxFloor = vilken översta våningen är
 		// TODO Auto-generated constructor stub
 		floors = nrfloors;
 		elev1 = new Elevator(floors,0);
 		elev2 = new Elevator(floors,0);
 		nrElevators = 2;
 
-		//FOR-loop fÃ¶r att initiera alla floors
+		//FOR-loop för att initiera alla floors
 		for (int j = 0; j < floors; j++) {
 			tempFloor = new Floor(j, (floors-1));
 			floorList.add(tempFloor);
--- src/Floor.java
+++ src/Floor.java
@@ -6,37 +6,41 @@ public class Floor {
 	private int index; //Vilken våning det är
 
 	//Buttons
-	private int btnup = 0; //0 = ej intryckt, 1 = intryckt
-	private int btndown = 0;//0 = ej intryckt, 1 = intryckt
+	private int btnUp = 0; //0 = ej intryckt, 1 = intryckt
+	private int btnDown = 0;//0 = ej intryckt, 1 = intryckt
 	
 	//Persons
 	private ArrayList<Person> people = new ArrayList<Person>();
 	
 	//Constructor
 	public Floor(int id, int maxFloor){
-		btnup = 0;
-		btndown = 0;
+		btnUp = 0;
+		btnDown = 0;
 		people = new ArrayList<Person>();
 		index = id;
 	}
 	
+	//Add an arriving person to the floor.
 	public void addPerson(Person p){
 		people.add(p);
 		if(p.getDestination()>index){
-			btnup = 1;
+			btnUp = 1;
 		}else if(p.getDestination()<index){
-			btndown = 1;
+			btnDown = 1;
 		}
 	}
 	
+	//Get a list of people who has been or is on the floor.
 	public ArrayList<Person> getPeople() {
         return this.people;
     }
 	
+	//Remove a person from the floor.
 	public void removePerson(Person p) {
         people.remove(p);
     }
 	
+	//Get a list of all the people 
 	public ArrayList<Person> getPeopleWaiting() {
         ArrayList<Person> waiting = new ArrayList<Person>();
         for (Person person : people){
@@ -45,6 +49,37 @@ public class Floor {
             }
         }
         return waiting;
+    }
+	
+	//Returns true if btnUp is pressed 
+	public boolean isbtnUpOn() {
+		if(btnUp == 1){
+			return true;
+		}
+        return false;
+    }
+	
+	//Returns true if btnDown is pushed
+    public boolean isbtnDownOn() {
+    	if(btnDown == 1){
+    		return true;
+    	}
+        return false;
+    }
+
+    //Set the btnUp to pushed(1)
+    public void setbtnUpOn() {
+    	btnUp = 1;
+    }
+
+    //set btnDown to pushed(1)
+    public void setbtnDownOn() {
+    	btnDown = 1;
+    }
+	
+	//Get which floor it is.
+	public int getFloor() {
+        return index;
     }
 	
 }
