fd3ff2d53dae40f19d9a7e3a1a7ae410064c8226
johanna <johannaaxelsson90@gmail.com>
2013-03-20 14:13:30 +0100

    more changes

--- src/Building.java
+++ src/Building.java
@@ -1,35 +1,33 @@
 import java.util.ArrayList;
 
+import org.apache.commons.math3.distribution.PoissonDistribution;
+
 public class Building {
-//represents a building 
+	//represents a building 
 
-  private int floors; // the number of floors in the building
-
-	private Elevator elev1;
-	private Elevator elev2;
-	private Floor tempFloor;
+	private int floors; // the number of floors in the building
+	private int id = 1;
 	private ArrayList<Floor> floorList;
 	private Elevator[] elevators= new Elevator[2]; 
 
 	public Building(int nrfloors, int nrElevators) { //id = vilken v�ning, maxFloor = vilken �versta v�ningen �r
-		// TODO Auto-generated constructor stub
 		floors = nrfloors;
 		elevators[0] = new Elevator(floors,0);
 		elevators[1] = new Elevator(floors,0);
 		nrElevators = elevators.length;
 
 		//FOR-loop f�r att initiera alla floors
+		Floor tempFloor;
 		for (int j = 0; j < floors; j++) {
 			tempFloor = new Floor(j, (floors-1));
 			floorList.add(tempFloor);
-
 		}
 
 		run();
 	}
 	public Person generatePerson(){
-		return new Person(floors, floors, floors);
-		//generates persons with a poisson distribution
+		return new Person(id, 2, 0);
+		//generates persons with help from a poisson distribution
 	}
 
 	private void run() {
@@ -37,21 +35,28 @@ public class Building {
 		BasicStrat Simple = new BasicStrat();
 		ZoneStrat Zone = new ZoneStrat();
 		ElevatorStrategy [] strategies={Simple , Zone};
-		
-		while (true){
+
+		PoissonDistribution generator = new PoissonDistribution(50);
+	
+
+		//eller for, med diskret tidssteg till en viss tid 
+		//while (true){
 			//generate person from poisson distribution
 			Person newPerson = generatePerson();
+			id++;
 			int handlingElevator = strategies[1].getElevator(newPerson.getPosition(), newPerson.getDestination(), elevators);
 			//adds the new request to the chosen elevator
 			elevators[handlingElevator].addToQueue(newPerson.getDestination());
-			
+			//request from outside or inside elevator
+			//så länge riktning stämmer gör det ingen skillnad, för att: annars plockar du bara upp folk på vägen
+
 			//decide which elevator will handle the request
 			//put the request (person) in queue for handlingElevator
 			//timestep 
 			for (Elevator elevator : elevators) {
-				elevator.timeStep();
+				elevator.timeStep(floorList);
 			}
-		}
+		//}
 
 	}
 
--- src/Elevator.java
+++ src/Elevator.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
+import java.util.PriorityQueue;
 public class Elevator {
-//Sarah test kommentar
-  //mappning från våningstryck till hissobjekt? 
+	//Sarah test kommentar
+	//mappning från våningstryck till hissobjekt? 
 	int floors = 1;
 	int entrance = 0;
 	//control panel 
@@ -16,6 +17,8 @@ public class Elevator {
 	//velocity while moving between levels
 	ArrayList<Boolean> buttonsPressed;
 	ArrayList<Person> personsInside;
+	//måste speca ordning på kön, använd .comparator
+	PriorityQueue queue = new PriorityQueue();
 	//entrance may not be needed
 	public Elevator(int floors, int entrance) {
 		for (int i = 0; i < floors; i++) {
@@ -24,25 +27,45 @@ public class Elevator {
 	}
 
 	public ArrayList<Boolean> getButtonsPressed(){
-		
+
 		return buttonsPressed;
 	}
-	
+
 	public void addToQueue(int dest){
+		queue.add(dest);
 		//adds the incoming request to the queue where destination is the drop/pickup place 
 		//check if request exists
 	}
 	// move the elevator a timestep
-	public void timeStep(){
+	public void timeStep(ArrayList<Floor> floorList){
+		queue.poll();
 		//if elevator stops at a floor, add some time (continue looping without action) to 
 		// simulate time for people entering/exiting the elevator
 		//EN kö för requests, såväl externa som interna. när man kommer till en våning med requests så
 		// kollar man om nån av personerna i hissen har det som request samt kollar om det floor man befinner sig
 		//på har folk som vill åka i rätt riktning
 		//get ON or get OFF, add or remove people in the elevator
-		if (buttonsPressed.get(this.currentlyAtFloor) == true /*or externa requests*/){
+		//for internal requests -> remove persons from elevator
+		if (queue.contains(this.currentlyAtFloor)==true ){
 			//om den befinner sig på ett floor där nån har tryckt på en knapp,
 			//så ska någon/några personer gå av 
+			personsInside.remove(null);
+		}
+		//for external requests -> add persons to elevator
+		Floor floor = floorList.get(currentlyAtFloor);
+		boolean floorDown = floor.isbtnDownOn();
+		boolean floorUp = floor.isbtnUpOn();
+		if ((floorDown == true && direction == 2)|(floorUp == true && direction == 1)){
+			floor = floorList.get(currentlyAtFloor);
+			ArrayList<Person> people = floor.getPeople();
+			for (Person person : people) {
+				person.getDestination();
+
+			}
+
+			//function for reaching the current floor
+			//floor.getPersons
+			personsInside.add(null);
 		}
 
 	}
